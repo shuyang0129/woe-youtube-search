438d42fec5efc45c9061532b86df871b9df697c0
shuyang0129 <shuyang0129@gmail.com>
2020-12-20 11:27:04 +0800

    Implement functionality of searchBar

--- src/pages/Home/components/SearchBar/index.jsx
+++ src/pages/Home/components/SearchBar/index.jsx
@@ -1,12 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import * as S from './style'
 
 const SearchBar = () => {
+  const [searchKeyword, setSearchKeyword] = useState('')
+
+  const updateSearchKeyword = e => {
+    // 防止開頭輸入空白
+    const input = e.target.value.replace(/^\s*/, '')
+
+    setSearchKeyword(input)
+  }
+
+  const submitSearchKeyword = () => {
+    // 1) 取得使用者欲搜尋的關鍵字
+    const keyword = searchKeyword.trim()
+    // 2) 清空輸入框
+    setSearchKeyword('')
+    // TODO: 搜尋
+    console.log(keyword)
+  }
+
   return (
     <S.SearchBarContainer>
       <S.SearchBar>
-        <S.SearchBarInput placeholder="Search by keywords" />
-        <S.SearchBarButton disabled />
+        <S.SearchBarInput
+          value={searchKeyword}
+          placeholder="Search by keywords"
+          onChange={updateSearchKeyword}
+        />
+        <S.SearchBarButton
+          disabled={searchKeyword.length === 0}
+          onClick={submitSearchKeyword}
+        />
       </S.SearchBar>
     </S.SearchBarContainer>
   )
