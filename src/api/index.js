efa5f4f9b822e2715697bae42f68b1135d31e68e
shuyang0129 <shuyang0129@gmail.com>
2020-12-20 15:35:42 +0800

    Fix searchApi function

--- src/api/index.js
+++ src/api/index.js
@@ -1,14 +1,17 @@
 import axios from 'axios'
 
+// YouTube Data API
 axios.defaults.baseURL = 'https://youtube.googleapis.com/youtube/v3'
 
 export const searchVideo = keyword => {
   return axios({
     url: '/search',
     params: {
-      part: keyword,
+      part: 'snippet',
       maxResults: 1,
       type: 'video',
+      eventType: 'completed',
+      q: encodeURIComponent(keyword), // URL-escaped when sent request
       key: process.env.REACT_APP_KEY,
     },
   })
