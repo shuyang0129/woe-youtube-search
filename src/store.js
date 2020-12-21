9dabe83c39a960c8c4e11dbe1fbe520718956368
shuyang0129 <shuyang0129@gmail.com>
2020-12-21 17:45:55 +0800

    Fix redux devtool issues

--- src/store.js
+++ src/store.js
@@ -8,9 +8,9 @@ const store = createStore(
   reducers,
   compose(
     applyMiddleware(thunk),
-    isDev &&
-      window.__REDUX_DEVTOOLS_EXTENSION__ &&
-      window.__REDUX_DEVTOOLS_EXTENSION__()
+    window.__REDUX_DEVTOOLS_EXTENSION__
+      ? window.__REDUX_DEVTOOLS_EXTENSION__()
+      : noop => noop
   )
 )
 
