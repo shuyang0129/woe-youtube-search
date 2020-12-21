8b517a7db471b38d0343cc43f2ed4d82aa9b0e00
shuyang0129 <shuyang0129@gmail.com>
2020-12-22 01:02:06 +0800

    Refactoring api function

--- src/api/index.js
+++ src/api/index.js
@@ -3,16 +3,26 @@ import axios from 'axios'
 // YouTube Data API
 axios.defaults.baseURL = 'https://youtube.googleapis.com/youtube/v3'
 
-export const searchVideo = keyword => {
+// Default params for searching video
+axios.defaults.params = {
+  part: 'snippet',
+  maxResults: 50,
+  type: 'video',
+  key: process.env.REACT_APP_KEY,
+}
+
+/**
+ * @name searchVideo
+ * @param {String} keyword
+ * @param {String|undefined} pageToken
+ * @description 利用YouTube Data API，搜尋YouTube影片
+ */
+export const searchVideo = (keyword, pageToken = undefined) => {
   return axios({
     url: '/search',
     params: {
-      part: 'snippet',
-      maxResults: 35,
-      type: 'video',
-      eventType: 'completed',
       q: keyword,
-      key: process.env.REACT_APP_KEY,
+      pageToken,
     },
   })
 }
